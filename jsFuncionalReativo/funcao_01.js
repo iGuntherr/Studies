eaa28d0938d35d7c75ffa362b548c60201f5a82d
iGuntherr <renan_eleto_viana@hotmail.com>
2021-09-16 19:46:24 -0300

    Add

---
+++ jsFuncionalReativo/funcao_01.js
@@ -0,0 +1,20 @@
+let a = 4
+console.log(4)
+
+// Function Declaration
+function bomDia(){
+    console.log('Bom dia!')
+}
+bomDia()
+
+// Function expression 
+const boaTarde = _ => console.log('Boa tarde!')
+boaTarde()
+
+function somar(a,b){
+    return a + b 
+}
+console.log(somar(5,20))
+
+const subtrair = (a,b) => a-b
+console.log(subtrair(15,6))
