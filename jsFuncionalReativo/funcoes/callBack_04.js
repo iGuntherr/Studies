7260dc915bcca55919d262da2a022db72d5360cb
iGuntherr <renan_eleto_viana@hotmail.com>
2021-09-18 21:09:48 -0300

    Add

--- jsFuncionalReativo/funcoes/callBack_04.js
+++ jsFuncionalReativo/funcoes/callBack_04.js
@@ -4,5 +4,14 @@ const carrinho = [
     { nome: 'Caderno', qtde: 4, preco: 27.10},
     { nome: 'Lapis', qtde: 3, preco: 5.82},
     { nome: 'Tesoura', qtde: 1, preco: 19.20},
-    { nome: 'Borracha', qtde: 10, preco: 0.50}
+    { nome: 'Borracha', qtde: 2, preco: 0.50}
 ]
+
+const getNome = obj => obj.nome
+const getTotal = obj => obj.preco * obj.qtde
+
+let carrinhoProd = carrinho.map(getNome)
+console.log(carrinhoProd)
+
+let carrinhoTotal = carrinho.map(getTotal)
+console.log(carrinhoTotal)
